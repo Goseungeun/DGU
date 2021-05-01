d71458cc9a8c02514d3c4edc836d8c5f86f3d7ef
Goseungeun <gotmddms@gmail.com>
2021-05-01 15:55:53 +0900

    modify graph detail

--- app/src/main/java/org/techtown/dgu/Graph.java
+++ app/src/main/java/org/techtown/dgu/Graph.java
@@ -2,6 +2,7 @@ package org.techtown.dgu;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +17,29 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
+class  MyValueFormatter extends ValueFormatter implements IValueFormatter {
+    private DecimalFormat mFormat;
+
+    public MyValueFormatter(){
+        mFormat = new DecimalFormat("##,###,##0.00");
+    }
+
+}
 
 public class Graph extends Fragment {
     LineChart chart;
@@ -44,7 +57,7 @@ public class Graph extends Fragment {
         //line graph 기본 설정
         chart = rootView.findViewById(R.id.graph_chart);
         chart.getDescription().setEnabled(false);
-        chart.setDrawGridBackground(true);
+        chart.setDrawGridBackground(false);
         chart.setBackgroundColor(Color.WHITE);
         chart.setViewPortOffsets(10f,10f,10f,10f);
 
@@ -62,29 +75,14 @@ public class Graph extends Fragment {
         xAxis.setAxisMaximum(9f);
         xAxis.setCenterAxisLabels(true);
         xAxis.setGranularity(1f);
-        /*
-        xAxis.setValueFormatter(new ValueFormatter() {
-
-            private final SimpleDateFormat mFormat = new SimpleDateFormat("MM-DD", Locale.KOREA);
-
-            @Override
-            public String getFormattedValue(float value) {
-                long millis = TimeUnit.HOURS.toMillis((long) value);
-                return mFormat.format(new Date(millis));
-            }
-        });
-
-         */
 
         //y축 설정
         YAxis leftAxis = chart.getAxisLeft();
-        //leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
         leftAxis.setTextColor(ColorTemplate.getHoloBlue());
         leftAxis.setDrawGridLines(true);
         leftAxis.setGranularityEnabled(true);
-        leftAxis.setAxisMinimum(0f);
-        leftAxis.setAxisMaximum(4.5f);
-        //leftAxis.setYOffset(9f);   //???
+        leftAxis.setAxisMinimum(1.5f);
+        leftAxis.setAxisMaximum(5f);
         leftAxis.setTextColor(Color.rgb(255,192,56));
 
         YAxis rightAxis = chart.getAxisRight();
@@ -104,20 +102,22 @@ private void setData(){
     // create a dataset and give it a type
     LineDataSet set1 = new LineDataSet(values, "DataSet 1");
     set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-    set1.setColor(ColorTemplate.getHoloBlue());
-    set1.setValueTextColor(ColorTemplate.getHoloBlue());
+    set1.setColor(Color.rgb(74,106,97));
+    set1.setValueTextColor(Color.BLACK);
     set1.setLineWidth(1.5f);
     set1.setDrawCircles(true);
     set1.setDrawValues(true);
     set1.setFillAlpha(65);
-    set1.setFillColor(ColorTemplate.getHoloBlue());
-    set1.setHighLightColor(Color.rgb(244, 117, 117));
+    set1.setFillColor(Color.rgb(74,106,97));
+    set1.setCircleColor(Color.rgb(74,106,97));
+    set1.setHighLightColor(Color.rgb(74,106,97));
     set1.setDrawCircleHole(false);
 
     // create a data object with the data sets
     LineData data = new LineData(set1);
     data.setValueTextColor(Color.BLACK);
-    data.setValueTextSize(9f);
+    data.setValueTextSize(15f);
+    data.setValueFormatter(new MyValueFormatter());
 
     // set data
     chart.setData(data);
