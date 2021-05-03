cfe1cee795806a2bad8318001fb990fe53e122a8
Ka Eul <kaeul991020@gmail.com>
2021-05-03 19:08:49 +0900

    implement backbutton

--- app/src/main/java/org/techtown/dgu/Timetable.java
+++ app/src/main/java/org/techtown/dgu/Timetable.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -90,7 +91,17 @@ public class Timetable extends Fragment {
         //타임테이블 보이기
         timetable.setAdapter(adapter);
 
+        //BackButton을 누르면 실행되는 함수
+        ImageButton backbutton = (ImageButton)view.findViewById(R.id.ttBackButton); // click시 Fragment를 전환할 event를 발생시킬 버튼을 정의합니다.
 
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // getActivity()로 MainActivity의 replaceFragment를 불러옵니다.
+                ((MainActivity)getActivity()).replaceFragment(new Home());    // 새로 불러올 Fragment의 Instance를 Main으로 전달
+            }
+        });
 
         return view;
 
