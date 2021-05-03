95bf3cf58ad3a299ef2a7c2031c93df3d704454d
Ka Eul <kaeul991020@gmail.com>
2021-05-03 23:54:45 +0900

    Create GraphTable

---
+++ app/src/main/java/org/techtown/dgu/GraphTable.java
@@ -0,0 +1,21 @@
+package org.techtown.dgu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GraphTable extends Fragment {
+    ViewGroup view;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = (ViewGroup) inflater.inflate(R.layout.home, container,false);
+
+        return view;
+    }
+}
