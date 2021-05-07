45475d94fc616a57d232a960392a6644b3989869
Ka Eul <kaeul991020@gmail.com>
2021-05-07 22:38:23 +0900

    Create SplashActivity

---
+++ app/src/main/java/org/techtown/dgu/SplashActivity.java
@@ -0,0 +1,24 @@
+package org.techtown.dgu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        try{
+            //1000=1초
+            Thread.sleep(4000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
