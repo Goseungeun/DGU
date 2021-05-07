7aa6d0aead87f438acadf00727b53fa13a19ddbb
Ka Eul <kaeul991020@gmail.com>
2021-05-07 23:41:26 +0900

    Add lottie animation

--- app/src/main/java/org/techtown/dgu/SplashActivity.java
+++ app/src/main/java/org/techtown/dgu/SplashActivity.java
@@ -3,8 +3,11 @@ package org.techtown.dgu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashActivity extends Activity {
     @Override
@@ -12,13 +15,18 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
-        try{
-            //1000=1초
-            Thread.sleep(4000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this,MainActivity.class));
-        finish();
+
+        LottieAnimationView lottie_animation =(LottieAnimationView) findViewById(R.id.animation_view);
+        lottie_animation.playAnimation();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },4000);
     }
 }
