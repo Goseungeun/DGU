4b6420252a5fb8d19b307d951d474825eec02e22
Ka Eul <kaeul991020@gmail.com>
2021-09-21 14:12:45 +0900

    Create stopwatch

--- app/src/main/java/org/techtown/dgu/stopwatch_Total.java
+++ app/src/main/java/org/techtown/dgu/stopwatch_Total.java
@@ -7,10 +7,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class Total_stopwatch  extends Fragment {
+public class stopwatch_Total extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.total_stopwatch, container, false); // Fragment로 불러올 xml파일을 view로 가져옵니다.
+        
+        //subject 관련 시간 더하기
+        
+        //license 관련 시간 더하기
+        
         return view;
     }
 
