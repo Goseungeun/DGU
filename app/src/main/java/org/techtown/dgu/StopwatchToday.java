15674e7470b4992af5afc3f779a4e067d66bdb62
Ka Eul <kaeul991020@gmail.com>
2021-10-12 03:03:54 +0900

    connection stopwatchToday and DB

--- app/src/main/java/org/techtown/dgu/StopwatchToday.java
+++ app/src/main/java/org/techtown/dgu/StopwatchToday.java
@@ -14,37 +14,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class StopwatchToday extends Fragment {
-    private Stopwatch_DB StopwatchDB;
+    private DGUDB DB;
     TextView Tv_time_total;
-    Handler handler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.total_stopwatch, container, false); // Fragment로 불러올 xml파일을 view로 가져옵니다.
 
-        StopwatchDB = new Stopwatch_DB(getContext());
+        DB = new DGUDB(getContext());
         //당일 총 공부시간 textview 연결
         Tv_time_total= view.findViewById(R.id.total_stopwatch);
 
-        handler.postDelayed(runnable, 0);
+        Tv_time_total.setText(DB.DateTotalStudyTime(DB.give_Today()));
 
         return view;
-    }
-    public final Runnable runnable = new Runnable() {
-
-        public void run() {
-            Tv_time_total.setText(StopwatchDB.getStudyTime(Today()));
-            handler.postDelayed(this, 0);
-        }
-
-    };
-    public String Today(){
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-        return sdf.format(date);
     }
 
 }
