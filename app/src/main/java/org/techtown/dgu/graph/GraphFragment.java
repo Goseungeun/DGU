54f5bc0c971381e61b28c76ce3c1c32685e08201
Ka Eul <kaeul991020@gmail.com>
2021-10-20 22:01:45 +0900

    Almost fix for Graph code

--- app/src/main/java/org/techtown/dgu/graph/GraphFragment.java
+++ app/src/main/java/org/techtown/dgu/graph/GraphFragment.java
@@ -1,6 +1,7 @@
 package org.techtown.dgu.graph;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,30 +11,37 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Utils;
 
 import org.techtown.dgu.DGUDB;
 import org.techtown.dgu.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class GraphFragment extends Fragment {
     private LineChart chart;
-    private ArrayList<Entry> values = new ArrayList<>();    //그래프에 표시할 값을 가지고 있는 arrayList
-    private final static int SEMESTER_NUM=9;//학기 개수
 
-    String[] semesterName={"1-1", "1-2", "2-1", "2-2" , "3-1", "3-2", "4-1", "4-2", "기타"};          //학기 이름 담을 리스트
-    float[] semester_score_list= new float[SEMESTER_NUM];    //학기별 평점 담을 리스트
+    //학기 이름 담을 리스트
+    private String[] semesterName={"1-1", "1-2", "2-1", "2-2" , "3-1", "3-2", "4-1", "4-2"};
 
     DGUDB DB;
+    int color;
 
 
     @Override
@@ -41,121 +49,145 @@ public class GraphFragment extends Fragment {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.graph,container,false);
 
         DB=new DGUDB(getContext());
+        color = getResources().getColor(R.color.deepgreen);
+        chart = view.findViewById(R.id.graph_chart);
 
-        initUI(view);
+        //차트 데이터 넣어주기
+        LineData lineData = getData();
+
+        //차트 꾸미기
+        setupChart(lineData);
 
         return view;
     }
 
-    private void initUI(ViewGroup rootView){
-        //line graph 기본 설정
-        chart = rootView.findViewById(R.id.graph_chart);
+    private LineData getData() {
+        ArrayList<Entry> values = new ArrayList<>();
 
-        chart.getDescription().setEnabled(false);
-        chart.setDrawGridBackground(false);
-        chart.setBackgroundColor(Color.WHITE);
-        chart.setViewPortOffsets(10f,10f,10f,10f);
-        chart.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.calendar_border));
-        chart.setPadding(15,15,15,15);
+        float score[] = DB.getGraph_gpa ();
 
-        ///
-        chart.setDragEnabled(true);
-        chart.setScaleEnabled(false);
-        ///
-
-        Legend legend = chart.getLegend();
-        legend.setEnabled(false);
-
-
-        //y축 설정
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
-        leftAxis.setDrawGridLines(true);
-        leftAxis.setGranularityEnabled(true);
-        leftAxis.setAxisMinimum(0.5f);
-        leftAxis.setAxisMaximum(5f);
-        leftAxis.setTextColor(Color.rgb(255,192,56));
-
-        YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-
-        //x축 설정
-        XAxis xAxis = chart.getXAxis();
-
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
-        xAxis.setTextSize(10f);
-        xAxis.setDrawAxisLine(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setTextColor(getResources().getColor(R.color.green));
-        xAxis.setAxisMinimum(0f);
-        xAxis.setAxisMaximum(10f);
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setGranularity(1f);
-
-        //x축 이름 설정
-        final ArrayList<String> xEntrys = new ArrayList<>();
-        for(int i=0;i<semesterName.length;i++) {
-            xEntrys.add(semesterName[i]);
+        //TODO 점수넣는곳
+        for (int i = 0; i < score.length; i++) {
+            values.add(new Entry((float)i, score[i]));
         }
-        chart.getXAxis().setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xEntrys));
 
-        //데이터 설정
-        setData();
-    }
-
-    private void setData(){
-        //데이터 입력하기
-        InputValues();
-
-        // create a dataset and give it a type
         LineDataSet set1 = new LineDataSet(values, "DataSet 1");
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set1.setColor(getResources().getColor(R.color.green));
-        set1.setValueTextColor(Color.BLACK);
-        set1.setLineWidth(2.5f);
-        set1.setDrawCircles(true);
-        set1.setDrawValues(true);
-        set1.setFillAlpha(65);
-        set1.setFillColor(getResources().getColor(R.color.green));
-        set1.setCircleColor(getResources().getColor(R.color.green));
-        set1.setHighLightColor(getResources().getColor(R.color.green));
-        set1.setDrawCircleHole(true);
-        set1.setCircleHoleRadius(3f);
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        set1.setLineWidth(1.75f);
         set1.setCircleRadius(5f);
+        set1.setCircleHoleRadius(2.5f);
+        set1.setColor(Color.WHITE);
+        set1.setCircleColor(Color.WHITE);
+        set1.setHighLightColor(Color.WHITE);
+        set1.setDrawValues(false);
 
+        // set color of filled area
+        // set the filled area
+        set1.setDrawFilled(true);
+        set1.setFillFormatter(new IFillFormatter() {
+            @Override
+            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                return chart.getAxisLeft().getAxisMinimum();
+            }
+        });
+        if (Utils.getSDKInt() >= 18) {
+            // drawables only supported on api level 18 and above
+            Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.fade_background);
+            set1.setFillDrawable(drawable);
+        } else {
+            set1.setFillColor(Color.BLACK);
+        }
 
-        ///
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
 
         // create a data object with the data sets
-        LineData data = new LineData(dataSets);
-        ///
-        data.setValueTextColor(Color.BLACK);
-        data.setValueTextSize(15f);
+        return new LineData(set1);
+    }
 
-        // set data
+
+    private void setupChart(LineData data) {
+
+        ((LineDataSet) data.getDataSetByIndex(0)).setCircleHoleColor(getResources().getColor(R.color.deepgreen));
+
+        // no description text
+        //아래에 조만하게 쓰인 글씨(설명)
+        chart.getDescription().setEnabled(false);
+
+        // chart.setDrawHorizontalGrid(false);
+        //
+        // enable / disable grid background
+        chart.setDrawGridBackground(false);
+//        chart.getRenderer().getGridPaint().setGridColor(Color.WHITE & 0x70FFFFFF);
+
+        // enable touch gestures
+        //touch가 먹는지 안먹는지
+        chart.setTouchEnabled(false);
+
+//        // enable scaling and dragging
+//        /*chart.setDragEnabled(true);
+//        chart.setScaleEnabled(true);
+//
+//        // if disabled, scaling can be done on x- and y-axis separately
+//        chart.setPinchZoom(false);*/
+//
+//        //설정안하면 투명한걸로 들어간다.
+//        //chart.setBackgroundColor(getResources().getColor(R.color.deepgreen));
+//
+//        // set custom chart offsets (automatic offset calculation is hereby disabled)
+//        //그래프와 축 사이 여백 설정
+//        //chart.setViewPortOffsets(10, 0, 10, 0);
+
+        // add data
         chart.setData(data);
-        //chart.invalidate();
-    }
 
-    //그래프에 들어갈 값을 입력한다.
-    private void InputValues() {
-        semester_score_list=DB.getGraph_gpa();
+        //범례표시 ( ex) 흰색은 dataset1임 같은거 표시하는거 )
+        // !!!get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+        l.setEnabled(false);
 
-        //values값 초기화
-        for(int i=0;i<values.size();i++){
-            values.remove(i);
-        }
-        //values에 값 넣기
-        for(int i=0;i<SEMESTER_NUM;i++){
-            float x_values =(float)(i+1);
-            if(semester_score_list[i]!=0f){
-                values.add(new Entry(x_values, (float) (Math.round(semester_score_list[i]*100)/100.00)));
+        //X축 꾸미기
+        {
+            XAxis xAxis = chart.getXAxis();
+            xAxis.setTextSize(11f);
+            xAxis.setTextColor(Color.WHITE);
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setDrawGridLines(false);
+            xAxis.setDrawAxisLine(false);
+            // create a dataset and give it a type
+
+            //X축에 넣을 말 정하기
+            final ArrayList<String> xEntrys = new ArrayList<>();
+            for(int i=0;i<semesterName.length;i++) {
+                xEntrys.add(semesterName[i]);
             }
+            chart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xEntrys));
+
         }
+
+        //Y축 꾸미기
+        {
+            YAxis leftAxis = chart.getAxisLeft();
+            leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+            leftAxis.setGridColor(Color.WHITE);
+            leftAxis.setAxisMaximum(4.5f);
+            leftAxis.setAxisMinimum(0f);
+            leftAxis.setDrawGridLines(true);
+
+
+            leftAxis.setGranularityEnabled(true);
+
+            leftAxis.setEnabled(true);
+            leftAxis.setSpaceTop(40);
+            leftAxis.setSpaceBottom(140);
+            chart.getAxisRight().setEnabled(false);
+        }
+
+        // animate calls invalidate()...
+        chart.animateX(1500);
+
     }
+
 
 }
 
