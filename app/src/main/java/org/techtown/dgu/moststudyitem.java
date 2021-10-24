179bd40417436c211bae1d8301f19cb21769fa11
Goseungeun <gotmddms@gmail.com>
2021-10-24 16:57:45 +0900

    Creat moststudy

---
+++ app/src/main/java/org/techtown/dgu/moststudyitem.java
@@ -0,0 +1,40 @@
+package org.techtown.dgu;
+
+public class moststudyitem {
+    String id;
+    String name;
+    String studytime;
+    String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudytime() {
+        return studytime;
+    }
+
+    public void setStudytime(String studytime) {
+        this.studytime = studytime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
